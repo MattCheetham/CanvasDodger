8317a41e58f300d316cdb7b7549ad6e8be6bfd4d
Matt Cheetham <m4ttcheetham@gmail.com>
2011-11-23 23:53:19 +0000

    Now handles player as object.

--- animate.js
+++ animate.js
@@ -1,53 +1,47 @@
 // JavaScript Document
 $(document).ready(function() {
 	/*
-	Define The Canvas Element
+	* Define The Canvas Element
 	*/
 	var canvas = $("#canvas");
 	
 	/*
-	Create the 2D drawing space
+	* Create the 2D drawing space
 	*/
 	var ctx = canvas.get(0).getContext("2d");
 	
 	/*
-	Define a variable for the canvas
-	width and height. This will help
-	us clear more effectively
+	* Define a variable for the canvas
+	* width and height. This will help
+	* us clear more effectively
 	*/
 	var canvasWidth = canvas.width();
 	var canvasHeight = canvas.height();
 	
 	/*
-	Define keys
-	WASD
+	* Define keys
+	* WASD
 	*/
     var keys = [ [ 37, 65 ], [ 39, 68 ] ];
     
     /*
-    Check if we have pressed a key
+    * Check if we have pressed a key
     */
     window.addEventListener('keydown',keyPress,true);
 	
 	/*
-	Work out which key we pressed and move the box in the right direction
+	* Work out which key we pressed and move the player in the right direction
 	*/
 	function keyPress(evt){
 		if( evt.which == keys[0][0] || evt.which == keys[0][1] ) {
-		x = x - 10;
+		player[0].x -= 10;
 	}else if( evt.which == keys[1][0] || evt.which == keys[1][1] ) {
-		x = x + 10;
+		player[0].x += 10;
 		}
 	}
 	
 	/*
-	Define where the player is by default
-	*/
-	var x = 0;
-	var y = 450;
-	
-	/*
-	Create a template for enemies
+	* Create a template for enemies
 	*/
 	var Enemies = function(x, y) {
 	this.x = x;
@@ -55,12 +49,25 @@ $(document).ready(function() {
 	};
 	
 	/*
-	Group the enemies
+	* Create a player
+	*/
+	var Player = function(x ,y) {
+	this.x = x;
+	this.y = y;
+	};
+	
+	var player = new Array();
+	
+	player.push(new Player(0, 450, 30, 30));
+	
+	/*
+	* Group the enemies
 	*/
 	var enemies = new Array();
 	
+	
 	/*
-	Spawn a new batch of enemies every 3 seconds
+	* Spawn a new batch of enemies every 3 seconds
 	*/
 	
 	function enemySpawn() {
@@ -75,21 +82,23 @@ $(document).ready(function() {
 
 	
 	/*
-	Animate the player movement
+	* Animate the player movement
 	*/
 	function animate() {
+	
+		var playerLength = player.length;
+		for (var i = 0; i < playerLength; i++){
+		var tmpPlayer = player[i];
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-		
 		ctx.fillStyle = "rgb(0, 0, 0)";
-		ctx.fillRect(x, y, 30, 30);
-		
-
+		ctx.fillRect(tmpPlayer.x, tmpPlayer.y, 30, 30);
+		};
 		
 		setTimeout(animate, 33);
 	}
 	
 	/*
-	Animate the enemies
+	* Animate the enemies
 	*/
 	
 	function enemyAnimate() {
@@ -105,7 +114,7 @@ $(document).ready(function() {
 	}
 	
 	/*
-	Actually run all of the animation functions
+	* Actually run all of the animation functions
 	*/
 	animate();
 	enemyAnimate();
