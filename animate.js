e25c273869d15f8ab5af1dc56ef4c51c1f0e826e
Matt Cheetham <m4ttcheetham@gmail.com>
2011-11-26 18:11:02 +0000

    Expanded canvas and inserted score board basics

--- animate.js
+++ animate.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	var direction = 'none';
 	var score = 0;
 	var enemySpeed = 1000;
-	var level = 0;
+	var level = 1;
 	var velocity = 3;
 	
 	/*
@@ -71,8 +71,8 @@ $(document).ready(function() {
 	* Create a template for enemies
 	*/
 	var Enemies = function(x, y) {
-	this.x = x;
-	this.y = y;
+		this.x = x;
+		this.y = y;
 	};
 	
 	/*
@@ -84,8 +84,8 @@ $(document).ready(function() {
 	* Create a player
 	*/
 	var Player = function(x ,y) {
-	this.x = x;
-	this.y = y;
+		this.x = x;
+		this.y = y;
 	};
 	
 	var player = new Array();
@@ -97,17 +97,21 @@ $(document).ready(function() {
 	*/
 	
 	function enemySpawn() {
-	enemies.push(new Enemies(Math.round(Math.random()*300), 0, 30, 30));
+	if(level == 1 || level == 2){
+			enemies.push(new Enemies(Math.round(Math.random()*270), 0, 30, 30));
+		} else if(level == 3){
+			enemies.push(new Enemies(Math.round(Math.random()*270), 0, 30, 30));
+			enemies.push(new Enemies(Math.round(Math.random()*270), 0, 30, 30));
+	}
+	
 	if(enemySpeed > 600 && level == 1){
-	enemySpeed -= 10;
-	console.log(enemySpeed)
-	} else if(enemySpeed > 300 && level == 2 && velocity > 10){
-	enemySpeed -= 5;
-	console.log(enemySpeed)
+			enemySpeed -= 10;
+		} else if(enemySpeed > 300 && level == 2 && velocity > 10){
+		enemySpeed -= 5;
 	}
 	
 	if(playAnimation){
-	setTimeout(enemySpawn, enemySpeed);
+		setTimeout(enemySpawn, enemySpeed);
 	}
 	}
 
@@ -119,10 +123,10 @@ $(document).ready(function() {
 	
 		var playerLength = player.length;
 		for (var i = 0; i < playerLength; i++){
-		var tmpPlayer = player[i];
-		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-		ctx.fillStyle = "red";
-		ctx.fillRect(tmpPlayer.x, tmpPlayer.y, 30, 30);
+			var tmpPlayer = player[i];
+				ctx.clearRect(0, 0, 300, 500);
+				ctx.fillStyle = "red";
+				ctx.fillRect(tmpPlayer.x, tmpPlayer.y, 30, 30);
 		};
 		
 		/*
@@ -131,12 +135,15 @@ $(document).ready(function() {
 		
 		var enemiesLength = enemies.length;
 		for (var i = 0; i < enemiesLength; i++) {
-		var tmpEnemies = enemies[i];
-		if( level == 1){
-		tmpEnemies.y += 3;
-		} else if( level == 2){
-		tmpEnemies.y += velocity;
+			var tmpEnemies = enemies[i];
+				if( level == 1){
+					tmpEnemies.y += 3;
+				} else if( level == 2){
+					tmpEnemies.y += velocity;
+				} else {
+					tmpEnemies.y += velocity;
 		}
+		
 		ctx.fillStyle = "black";
 		ctx.fillRect(tmpEnemies.x, tmpEnemies.y, 30, 30);
 		
@@ -144,9 +151,9 @@ $(document).ready(function() {
 		* look for collisions
 		*/
 		if (!(tmpEnemies.x+30 < player[0].x) &&
-		!(player[0].x+30 < tmpEnemies.x) &&
-		!(tmpEnemies.y+30 < player[0].y) &&
-		!(player[0].y+30 < tmpEnemies.y)) { 
+			!(player[0].x+30 < tmpEnemies.x) &&
+			!(tmpEnemies.y+30 < player[0].y) &&
+			!(player[0].y+30 < tmpEnemies.y)) { 
 		playAnimation = false;
 		};
 		
@@ -154,7 +161,7 @@ $(document).ready(function() {
 		* Clears the rectangle once it has left the screen
 		*/
 		if (tmpEnemies.y > 500){
-		ctx.clearRect(tmpEnemies.x, tmpEnemies.y, 30, 30)
+			ctx.clearRect(tmpEnemies.x, tmpEnemies.y, 30, 30)
 		};
 		
 		};
@@ -163,60 +170,102 @@ $(document).ready(function() {
 		* Move the player
 		*/
 		if( direction == 'left'){
-			if(player[0].x < 5){
-			player[0].x == 5;
+			if(player[0].x-8 < 0){
+				player[0].x = 0;
 			} else {
-			player[0].x -= 8;
+				player[0].x -= 8;
 			}
 		} else if( direction == 'right'){
-			if(player[0].x > 270){
-			player[0].x == 270;
+			if(player[0].x+8 > 270){
+				player[0].x = 270;
 			} else {
-			player[0].x += 8;
+				player[0].x += 8;
 			}
 		}
-
-		/*
-		* Show the score
-		*/
-		ctx.font = '20px san-serif';
-		ctx.textBaseline = 'bottom';
-		ctx.fillText(score, 270, 485);
-		
+	
 		if(playAnimation){
-		setTimeout(animate, 33);
+			setTimeout(animate, 33);
 		}
 	}
 	
-		function countScore() {
-	
 		/*
-		* Count our score and change level if needed
+		* Count our score
 		*/
+		function countScore() {
 		score += 1;
 		
 		if(playAnimation){
-		setTimeout(countScore, 500);
+			setTimeout(countScore, 500);
 		}
 	}
 	
+		/*
+		* Change level depending on score
+		*/
 		function levelCheck(){
 				
 		if(score < 75){
-		level = 1;
-		} else if(score < 130){
-		level = 2;
+			level = 1;
+		} else if(score < 200){
+			level = 2;
+		} else if(score < 300){
+			level = 3;
 		}
 		
 		if( level == 2 && velocity < 10 ){
-		velocity += 0.01;
+			velocity += 0.01;
 		}
 		
 		if(playAnimation){
-		setTimeout(levelCheck, 33);
+			setTimeout(levelCheck, 33);
 		}
 		
 		}
+		
+	/*****************************/
+	/*****************************/
+	/*****Score Board Section*****/
+	/*****************************/
+	/*****************************/
+	
+	/*
+	* Draw the board
+	*/
+	function drawBoard(){
+		ctx.fillStyle = "black";
+		ctx.beginPath();
+		ctx.moveTo(300,0);
+		ctx.lineTo(300,500);
+		ctx.lineTo(301,500);
+		ctx.lineTo(301,0);
+		ctx.fill();
+		
+		ctx.font = '40px san-serif';
+		ctx.textBaseline = 'bottom';
+		ctx.fillText("Level:", 325, 400);
+		ctx.fillText("Score:", 325, 470);
+		}
+		
+	/*
+	* Show the score & Level
+	*/
+	function showScore(){
+		ctx.clearRect(325, 465, 100, 30);
+		ctx.font = '25px san-serif';
+		ctx.textBaseline = 'bottom';
+		if(score < 10){
+		ctx.fillText(score, 370, 490);
+		} else if(score <100){
+		ctx.fillText(score, 365, 490);
+		}
+		
+		ctx.clearRect(325, 395, 100, 30);
+		ctx.fillText(level, 370, 420);
+		
+		if(playAnimation){
+			setTimeout(showScore, 33);
+		}
+	}
 	
 	/*
 	* Actually run all of the animation functions
@@ -225,4 +274,6 @@ $(document).ready(function() {
 	enemySpawn();
 	countScore();
 	levelCheck();
+	drawBoard();
+	showScore();
 });
